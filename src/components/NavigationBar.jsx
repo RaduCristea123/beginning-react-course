3e15d205c4c59ba95426b2c089aedcc4e44fe7e6
Radu Cristea <cristea.radu23@gmail.com>
2024-04-08 19:16:27 +0300

    ep-12 part-1

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function NavigationBar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/" activeClassName="active">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" activeClassName="active">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact" activeClassName="active">Contact</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
---
+++ src/components/Root.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import App from './App';
+import About from './pages/About';
+import NavigationBar from './NavigationBar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Contact from './Contact';
+
+export default function Root() {
+  return (
+    <Router>
+      <div className="todo-app-container">
+        <NavigationBar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<App />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
